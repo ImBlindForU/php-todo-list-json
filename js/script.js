0072293f7a92db65e46471070a247f4aff271015
ImBlindForU <samuele.zanchi10@gmail.com>
2022-11-29 17:32:04 +0100

    server.php addtask

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ createApp({
   data() {
     return {
       todoslist:[],
-      newTask:[],
+      newTask:"",
 
     };
   },
--- server.php
+++ server.php
@@ -3,6 +3,17 @@
 // prendo i dati e li stampo
 $string= file_get_contents("todo.json");
 echo $string;
+$tasks = json_decode($string, true);
 // invio dati
-
+if (isset($_POST["newTask"])) {
+    // Siamo nel caso di salvataggio del nuovo dato
+    $new_task = $_POST["newTask"];
+    
+    echo $new_task;
+    $newtask = [
+        "text" => $new_task,
+        "done" => false
+    ];
+    $tasks[]= $newtask;
+};
 header("content-type: application/json");
